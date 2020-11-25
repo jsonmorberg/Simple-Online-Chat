9019f40d9524c987238e09c7559ab5a472e89b43
Jayson <jaysonmorberg9@gmail.com>
2020-11-24 18:29:23 -0800

    formatted & cleaned up code

--- observer.c
+++ observer.c
@@ -1,4 +1,4 @@
-/* observer.c - code for observer. Do not rename this file */
+/*observer.c - code for observer. Do not rename this file */ 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,152 +12,156 @@
 #include <poll.h>
 
 int checkWord(char *word){
-    int len = strlen(word);
-    
-    if(len < 1 || len > 10){
-        return 0;
-    } 
+	int len = strlen(word);
 
-    for(int i = 0; i < len; i++){
-        char letter = word[i];
-        if(!isalpha(letter) && letter != '_' && !isdigit(letter)){
-            return 0;
-        }
-    }    
-    
-    return 1;
+	if (len < 1 || len > 10){
+		return 0;
+	}
+
+	for (int i = 0; i < len; i++){
+		char letter = word[i];
+		if (!isalpha(letter) && letter != '_' && !isdigit(letter)){
+			return 0;
+		}
+	}
+
+	return 1;
 }
 
 void observer(int sd){
 
-    struct pollfd mypoll = {STDIN_FILENO, POLLIN|POLLPRI}; 
-    int timeout = 60 * 1000;
+	struct pollfd mypoll = { 
+        STDIN_FILENO, POLLIN | POLLPRI
+	};
+	int timeout = 60 * 1000;
 
-    char response;
-    recv(sd, &response, sizeof(char), 0);
+	char response;
+	recv(sd, &response, sizeof(char), 0);
 
-    if(response == 'N'){
-        printf("Server is full\n");
-        return;
-    }
+	if (response == 'N'){
+		printf("Server is full\n");
+		return;
+	}
 
-    char buf[100];
-    printf("Enter a participant's username: ");
-    fflush(stdout);
+	printf("Enter a participant's username: ");
+	fflush(stdout);
 
-    while(1){
+	while (1){
+		//poll will block till user input or timeout
 
-        if(poll(&mypoll, 2, timeout)){
-            scanf("%s", buf);
-            
+		if (poll(&mypoll, 1, timeout)){
+			char *buf = NULL;
+			size_t length = 0;
+			getline(&buf, &length, stdin);
 
-            if(checkWord(buf)){
-                uint8_t wordLength = strlen(buf);
-                buf[wordLength] = ' ';
-                send(sd, &wordLength, sizeof(uint8_t), MSG_NOSIGNAL);
-                send(sd, buf, wordLength, MSG_NOSIGNAL);
+			if ((strlen(buf) > 0) && (buf[strlen(buf) - 1] == '\n')){
+				buf[strlen(buf) - 1] = '\0';
+			}
 
-                if(recv(sd, &response, sizeof(char), 0) == 0){
-                    return;
-                }
+			if (checkWord(buf)){
+				uint8_t wordLength = strlen(buf);
+				buf[wordLength] = ' ';
+				send(sd, &wordLength, sizeof(uint8_t), MSG_NOSIGNAL);
+				send(sd, buf, wordLength, MSG_NOSIGNAL);
 
-                if(response == 'Y'){
-                    break;
-                }else if(response == 'T'){
-                    printf("That participant already has an observer. Enter a different participant's username: ");
-                    fflush(stdout);
-                }else if(response == 'N'){
-                    printf("There is no active participant with that username.\n");
-                    return;
-                }
+				if (recv(sd, &response, sizeof(char), 0) == 0){
+					return;
+				}
 
-            }else{
-               printf("Enter a participant's username: ");
-               fflush(stdout);
-            }
+				if (response == 'Y'){
+					break;
+				}else if (response == 'T'){
+					printf("That participant already has an observer. Enter a different participant's username: ");
+					fflush(stdout);
+				}else if (response == 'N'){
+					printf("There is no active participant with that username.\n");
+					return;
+				}
+			}else{
+				printf("Enter a participant's username: ");
+				fflush(stdout);
+			}
+		}else{
+			printf("Time to enter a username has expired.\n");
+			return;
+		}
+	}
 
-        }else{
-            printf("Time to enter a username has expired.\n");
-            return;
-        }
-    }
+	for (;;){
+		uint16_t messageLength = 0;
+		if (recv(sd, &messageLength, sizeof(uint16_t), MSG_WAITALL) == 0){
+			return;
+		}
 
-    for(;;){
-        
-        uint16_t messageLength = 0;
-        if(recv(sd, &messageLength, sizeof(uint16_t), MSG_WAITALL) == 0){
-            return;
-        }
-        messageLength = ntohs(messageLength);
+		messageLength = ntohs(messageLength);
 
-        char message[messageLength + 1];
-        if(recv(sd, message, messageLength, MSG_WAITALL) == 0){
-            return;
-        }
-        message[messageLength] = '\0';
+		char message[messageLength + 1];
+		if (recv(sd, message, messageLength, MSG_WAITALL) == 0){
+			return;
+		}
 
-        printf("%s\n", message);
-    }
+		message[messageLength] = '\0';
+		printf("%s\n", message);
+	}
 }
 
-int main(int argc, char **argv) {
-    struct hostent *ptrh; /* pointer to a host table entry */
-    struct protoent *ptrp; /* pointer to a protocol table entry */
-    struct sockaddr_in sad; /* structure to hold an IP address */
-    int sd; /* socket descriptor */
-    int port; /* protocol port number */
-    char *host; /* pointer to host name */
+int main(int argc, char **argv){
+	struct hostent * ptrh; /*pointer to a host table entry */
+	struct protoent * ptrp; /*pointer to a protocol table entry */
+	struct sockaddr_in sad; /*structure to hold an IP address */
+	int sd; /*socket descriptor */
+	int port; /*protocol port number */
+	char *host; /*pointer to host name */
 
-    memset((char *) &sad, 0, sizeof(sad)); /* clear sockaddr structure */
-    sad.sin_family = AF_INET; /* set family to Internet */
+	memset((char*) &sad, 0, sizeof(sad)); /*clear sockaddr structure */
+	sad.sin_family = AF_INET; /*set family to Internet */
 
+	if (argc != 3){
+		fprintf(stderr, "Error: Wrong number of arguments\n");
+		fprintf(stderr, "usage:\n");
+		fprintf(stderr, "./client server_address server_port\n");
+		exit(EXIT_FAILURE);
+	}
 
-    if (argc != 3) {
-        fprintf(stderr, "Error: Wrong number of arguments\n");
-        fprintf(stderr, "usage:\n");
-        fprintf(stderr, "./client server_address server_port\n");
-        exit(EXIT_FAILURE);
-    }
+	port = atoi(argv[2]); /*convert to binary */
+	if (port > 0) /*test for legal value */
+		sad.sin_port = htons((u_short) port);
+	else{
+		fprintf(stderr, "Error: bad port number %s\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
 
-    port = atoi(argv[2]); /* convert to binary */
-    if (port > 0) /* test for legal value */
-        sad.sin_port = htons((u_short) port);
-    else {
-        fprintf(stderr, "Error: bad port number %s\n", argv[2]);
-        exit(EXIT_FAILURE);
-    }
+	host = argv[1]; /*if host argument specified */
 
-    host = argv[1]; /* if host argument specified */
+	/*Convert host name to equivalent IP address and copy to sad. */
+	ptrh = gethostbyname(host);
+	if (ptrh == NULL){
+		fprintf(stderr, "Error: Invalid host: %s\n", host);
+		exit(EXIT_FAILURE);
+	}
 
-    /* Convert host name to equivalent IP address and copy to sad. */
-    ptrh = gethostbyname(host);
-    if (ptrh == NULL) {
-        fprintf(stderr, "Error: Invalid host: %s\n", host);
-        exit(EXIT_FAILURE);
-    }
+	memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
 
-    memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
+	/*Map TCP transport protocol name to protocol number. */
+	if (((long int)(ptrp = getprotobyname("tcp"))) == 0){
+		fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Map TCP transport protocol name to protocol number. */
-    if (((long int) (ptrp = getprotobyname("tcp"))) == 0) {
-        fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
-        exit(EXIT_FAILURE);
-    }
+	/*Create a socket. */
+	sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+	if (sd < 0){
+		fprintf(stderr, "Error: Socket creation failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Create a socket. */
-    sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
-    if (sd < 0) {
-        fprintf(stderr, "Error: Socket creation failed\n");
-        exit(EXIT_FAILURE);
-    }
+	/*Connect the socket to the specified server. */
+	if (connect(sd, (struct sockaddr *) &sad, sizeof(sad)) < 0){
+		fprintf(stderr, "connect failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Connect the socket to the specified server. */
-    if (connect(sd, (struct sockaddr *) &sad, sizeof(sad)) < 0) {
-        fprintf(stderr, "connect failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    observer(sd);
-    close(sd);
-    exit(EXIT_SUCCESS);
+	observer(sd);
+	close(sd);
+	exit(EXIT_SUCCESS);
 }
